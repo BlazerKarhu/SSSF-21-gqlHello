87fab21cae8edeebd09307f9030ca889eab46059
Joonas Mielonen <Joonas.mielonen@metropolia.fi>
2021-03-29 12:00:32 +0300

    db connection

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ import {ApolloServer} from 'apollo-server-express';
 import schemas from './schemas/index.js';
 import resolvers from './resolvers/index.js';
 import express from 'express';
+import dotenv from 'dotenv';
+import connectMongo from './db/db.js';
+
+dotenv.config();
 
 (async () => {
   try {
+    const conn = await connectMongo();
+    if (conn) {
+      console.log('Connected successfully.');
+    }
+
     const server = new ApolloServer({
       typeDefs: schemas,
       resolvers,
