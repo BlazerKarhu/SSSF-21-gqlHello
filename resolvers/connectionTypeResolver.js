7869fd1d0f3a88dc3554b7bebea94ea796b5e029
Joonas Mielonen <Joonas.mielonen@metropolia.fi>
2021-04-12 17:22:43 +0300

    update

--- resolvers/connectionTypeResolver.js
+++ resolvers/connectionTypeResolver.js
@@ -3,6 +3,9 @@ import ConnectionType from '../models/connectionType.js';
 export default {
   Query: {
     connectiontypes: (parent, args, {user}) => {
+      if (!user) {
+        throw new AuthenticationError('You are not authenticated');
+      }
       return ConnectionType.find();
     },
   },
